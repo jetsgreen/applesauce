a9fb605755e4f318e5382da474fe59b8a7bda620
Felix Fernandez <fernandezfelix460.gmail.com>
2019-10-10 21:25:58 -0400

    pushing intial folder

---
+++ assets/javascript/game.js
@@ -0,0 +1,23 @@
+// 1 Create a function that generates a different random # at the start of each game
+// (the number has to be between 19 and 120)
+// 2 Create a function for all the crystal options that generate a random number
+// (between 1 and 12) and adds them up until you either match the randomly
+//  generated number(win) or go over it(lost)
+// 3 Create a function that tracks wins and loses and 
+//  that alerts user when he's either won or lost
+// 4 Create a function that adds the points as you click the 
+// crystals to try to guess the random number
+
+$(document).ready(function() {
+
+
+    var red = 0;
+    var blue = 0;
+    var yellow = 0;
+    var purple = 0;
+    
+    $( "#red" ).click(function() {
+        alert( "its working" );
+      });
+
+});
